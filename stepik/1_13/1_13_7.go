db22ade765191daa4e7d99770c743cea2f268101
Павел Люльченко <graygolem@gmail.com>
2020-11-13 17:05:24 +0500

    Fixes #5

--- stepik/1_13/1_13_7.go
+++ stepik/1_13/1_13_7.go
@@ -4,15 +4,11 @@ import "fmt"
 
 // По данным числам, определите количество чисел, которые равны нулю.
 func main() {
-	var n int
+	var n, b, c int
 	fmt.Scan(&n)
-	s := make([]int, n)
 	for i := 0; i < n; i++ {
-		fmt.Scan(&s[i])
-	}
-	var c int
-	for _, v := range s {
-		if v == 0 {
+		fmt.Scan(&b)
+		if b == 0 {
 			c++
 		}
 	}
